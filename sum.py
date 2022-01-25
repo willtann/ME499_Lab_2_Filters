0325b609f25f72efe0a3af6937a12c642e6f224d
Tanner WIlliams <97181243+willtann@users.noreply.github.com>
2022-01-24 19:45:25 -0800

    Monday 1.24.22

--- sum.py
+++ sum.py
@@ -1,17 +1,32 @@
 #! /Users/tannerwilliams/Desktop/ME 499/ME499_Lab_2_Filters
 import math
+import random
 
 
 def sum_i(l1):
     counter = 0
+    x = 0
     #  for each element in the list add it to the total
     y = int(len(l1))
-    for x in range(y):
-        counter.append(l1(x))
+    for x in range(y):  # count up and add each iteration to total
+        counter += l1[x]
+        x += 1
     return counter
 
 
-l1 = [1, 4, 5]
-print(sum_i(l1))
+def sum_r(l1):
+    # If list is longer than one number there is a sum to  be found
+    if len(l1) > 1:
+        return l1[0] + sum_r(l1[1:])
+    else:
+        return l1[0]  # no sum... just the integer itself
 
-print(sum(l1))
+
+if __name__ == "__main__":
+    l1 = []
+    # 100 examples testing against our custom factorial function
+    for i in range(100):
+        l = random.randint(0, 15) # get random int each time
+        l1.append(l)  # making a list
+        assert sum_i(l1) == sum_r(l1)  # test each number in each function
+
