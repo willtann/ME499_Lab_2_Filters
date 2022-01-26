9ff90fa71b460a2178d48f40bb9fed39bf573803
Tanner WIlliams <97181243+willtann@users.noreply.github.com>
2022-01-26 11:39:01 -0800

    Wednesday 1.26.22

---
+++ amp_filter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+def apply_amp_filter(data, cutoff=0.8):
+    """
+    Apply an amplitude filter to an iterable, returning a list.
+    """
+
+    # Make an empty list
+    filtered = []
+
+    # Add one element at a time to the list.  There are a lot of ways to do this; this is just one of them.
+    for datum in data:
+        datum = min(datum, cutoff)
+        filtered.append(datum)
+    
+    # Return the new list
+    return filtered
--- sensor.py
+++ sensor.py
@@ -1,1 +1,22 @@
-#! /Users/tannerwilliams/Desktop/ME 499/ME499_Lab_2_Filters
+#!/usr/bin/env python3
+
+
+from math import sin
+from random import gauss
+
+
+def generate_sensor_data(n, sigma=0.01):
+    """
+    Generate data from a fake sensor.  These data are generated according to a sine function, with zero-mean
+    Gaussian noise, with standard deviation sigma.
+    """
+    data = []
+    for i in range(n):
+        data.append(sin(i * 0.01) + gauss(0.0, sigma))
+    return data
+
+
+if __name__ == '__main__':
+    # Print out a few of the numbers
+    for d in generate_sensor_data(10):
+        print(d)
