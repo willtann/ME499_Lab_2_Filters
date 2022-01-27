408bee4346d387846fcde251b6f391c6c657ab8b
Tanner WIlliams <97181243+willtann@users.noreply.github.com>
2022-01-26 19:56:00 -0800

    Wednesday 1.26.22

--- reverse.py
+++ reverse.py
@@ -3,23 +3,26 @@ import math
 import random
 
 
-def reverse_i(r1):
-    r2 = []  # make an empty list
-    l = len(r1) - 1  #
-    for i in range(len(r1)):
-        r2.append(r1[l - i])
-    return r2
+def reverse_i(r):
+    r1 = []  # make an empty list
+    l = len(r) - 1  # since index @0
+    for i in range(len(r)):
+        r1.append(r[l - i])
+    return r1
 
 
-def reverse_r(r1):
-    reverse = lambda r1: (reverse(r1[1:]) + r1[:1] if r1 else [])
-    return reverse(r1)
-
+def reverse_r(r2):
+    list = lambda r2: (list(r2[1:]) + r2[:1] if r2 else [])
+    return list(r2)
+    # if r2:
+    #     rev_list = lambda r2: (rev_list(r2[1:])) + r2[:1]
+    #     return rev_list(r2)
+    # else:
+    #     rev_list = lambda r2: (rev_list(r2[1:])) + []
+    #     return rev_list(r2)
 
 if __name__ == "__main__":
-    r1 = [1, 2, 3, 4, 5, 6, 7, 8, 'yes']
+    r = [1, 2, 3, 4, 5, 6, 7, 8, 'yes']
 
-    print(reverse_i(r1))
-    print(reverse_r(r1))
-
-
+    print(reverse_i(r))
+    print(reverse_r(r))
