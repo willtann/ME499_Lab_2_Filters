54907b8c8ce2b80e95ae4a2d53a26dc5f7e8ae15
Tanner WIlliams <97181243+willtann@users.noreply.github.com>
2022-01-26 22:44:40 -0800

    Wednesday 1.26.22

--- filters.py
+++ filters.py
@@ -1,5 +1,4 @@
 #! /Users/tannerwilliams/Desktop/ME 499/ME499_Lab_2_Filters
-import numpy
 
 
 def mean_filter(data, filter_width=3):  # default 3 if not specified
@@ -9,13 +8,21 @@ def mean_filter(data, filter_width=3):  # default 3 if not specified
 
     if odd == 1 and filter_width > 0:  # odd and positive width
         # List with the averages (shorter than original)
-        m_filter = [0] * (len(data) - (filter_width - 1))  # empty list of size
+        m_filter = [filter_width] * (len(data) - (filter_width - 1))  # empty list of size
         print('New data length = ', len(m_filter))
+        l_data = len(data)
 
-        # Using numpy to calculate cumulative sum
-        sum_calc = numpy.cumsum(data, dtype=list)
+        # Calculate cumulative sum for each set with function
+        def sum_c(data):
+            sum_data = data.copy()  # Leave data alone
+            for i in range(1, len(data)):
+                sum_data[i] = sum_data[i] + sum_data[i - 1]
+            return sum_data
+
+        sum_calc = sum_c(data)
         # Refining cumulative sum to filter_width
-        sum_calc[filter_width:] = sum_calc[filter_width:] - sum_calc[:-filter_width]
+        # zipping to avoid type discrepancies
+        sum_calc[filter_width:] = [a - b for a, b in zip(sum_calc[filter_width:] , sum_calc[:-filter_width])]
         # Divide sum of index by filter width to find mean
         return sum_calc[filter_width - 1:] / filter_width
 
@@ -25,4 +32,4 @@ def mean_filter(data, filter_width=3):  # default 3 if not specified
 
 
 if __name__ == "__main__":
-    print(mean_filter([1, 2, 3, 2, 1, 2, 3, 2, 1, 2, 3, 2, 1]))
+    print(mean_filter([1, 2, 3, 2, 1, 2, 3, 2, 1, 2, 3, 2, 1], 3))
